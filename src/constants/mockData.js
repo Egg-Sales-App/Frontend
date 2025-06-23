1e76f425fc6aa58b2288ee6736ad4a3393eb6012
Saintdannyyy <danieltesla746@gmail.com>
2025-06-23 07:50:18 +0000

    add mock data for sales overview, purchase overview, top selling products, and low stock items

---
+++ src/constants/mockData.js
@@ -0,0 +1,42 @@
+export const SALES_OVERVIEW_DATA = {
+  sales: 832,
+  revenue: 1300,
+  profit: 868,
+  cost: 1432,
+};
+
+export const PURCHASE_OVERVIEW_DATA = {
+  purchase: 832,
+  cost: 1300,
+  cancel: 868,
+  return: 1432,
+};
+
+export const TOP_SELLING_PRODUCTS = [
+  {
+    name: "Day old chicks",
+    soldQuantity: 30,
+    remainingQuantity: 12,
+    price: 100,
+  },
+  { name: "Broilers", soldQuantity: 21, remainingQuantity: 15, price: 207 },
+  { name: "Dewormers", soldQuantity: 19, remainingQuantity: 17, price: 105 },
+];
+
+export const LOW_STOCK_ITEMS = [
+  {
+    name: "Chicken Feed",
+    image: "/assets/chicken_feed.png",
+    remainingQuantity: "10 Packet",
+  },
+  {
+    name: "Broilers",
+    image: "/assets/broilers.jpeg",
+    remainingQuantity: "15 Packet",
+  },
+  {
+    name: "Day old chicks",
+    image: "/assets/day_old_chicks.jpg",
+    remainingQuantity: "15 Packet",
+  },
+];
